9db4cb399f743aa004f806a0ba278deef462ea88
Derrick Southworth <derricksouthworth@gmail.com>
2021-01-03 16:26:06 -0700

    Create Note List and Note Add/Edit activities.  Closes #1

---
+++ app/src/main/java/com/example/academicprogresstracker/UI/NoteAddEditActivity.java
@@ -0,0 +1,16 @@
+package com.example.academicprogresstracker.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.academicprogresstracker.R;
+
+public class NoteAddEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_add_edit);
+    }
+}
